a482f8c4e6848cf6b259d43232c234c833f07a94
dadiyang <dadiyang@aliyun.com>
2018-12-19 14:23:12 +0800

    发布v1.0.2

--- src/main/java/it/sauronsoftware/jave/AudioUtils.java
+++ src/main/java/it/sauronsoftware/jave/AudioUtils.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (C) 2018 Zhejiang xiaominfo Technology CO.,LTD.
- * All rights reserved.
- * Official Web Site: http://www.xiaominfo.com.
- * Developer Web Site: http://open.xiaominfo.com.
- */
-
 package it.sauronsoftware.jave;
 
 import java.io.File;
