fe3f0889e2b5efd37236cf75d909d639e085db9c
dadiyang <dadiyang@aliyun.com>
2019-03-08 22:08:59 +0800

    添加时长获取单元测试;发布1.0.4

--- src/test/java/it/sauronsoftware/jave/JaveTests.java
+++ src/test/java/it/sauronsoftware/jave/JaveTests.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.assertTrue;
  */
 public class JaveTests {
     private File source = new File("target/test-classes/material/testAudio.amr");
+    private File mp3Source = new File("target/test-classes/material/testAudio.mp3");
 
     @Test
     public void amrToMp3() {
@@ -26,5 +27,21 @@ public class JaveTests {
         File wavTarget = new File("testAudio.wav");
         AudioUtils.amrToWav(source, wavTarget);
         assertTrue(wavTarget.getAbsolutePath() + " must exists.", wavTarget.exists());
+    }
+
+    @Test
+    public void getDuration() {
+        Encoder encoder = new Encoder();
+        try {
+            MultimediaInfo amrInfo = encoder.getInfo(source);
+            System.out.println("amr duration:" + amrInfo.getDuration());
+            assertTrue("Duration of the amr source file must greater than 0", amrInfo.getDuration() > 0);
+
+            MultimediaInfo mp3Info = encoder.getInfo(mp3Source);
+            System.out.println("mp3 duration: " + mp3Info.getDuration());
+            assertTrue("Duration of the mp3 source file must greater than 0", mp3Info.getDuration() > 0);
+        } catch (EncoderException e) {
+            e.printStackTrace();
+        }
     }
 }
